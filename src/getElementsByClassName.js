99d3716d3caf1c9112d0893efbb00eb631546715
Xiaozhou Jia <jiaxiaozhou@gmail.com>
2018-04-11 15:41:06 +0900

    Finished getElementsByClassName method

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,4 +1,17 @@
 // eslint-disable-next-line no-unused-vars
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (targetClassName) => {
+  const result = [];
+
+  const recursionHelper = function(domNode) {
+    if (domNode && domNode.classList.contains(targetClassName)) {
+      result.push(domNode);
+    }
+    const nodes = domNode.children;
+    for (let i = 0; i < nodes.length; i++) {
+      recursionHelper(nodes[i]);
+    }
+  };
+
+  recursionHelper(document.body);
+  return result;
 };
